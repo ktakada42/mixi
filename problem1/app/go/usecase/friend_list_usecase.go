58a4e627ce959b700e99a595672b7e876983f65d
ktakada42 <ktakada950711@gmail.com>
2022-09-03 11:38:17 +0900

    feat: create usecase layer

---
+++ problem1/app/go/usecase/friend_list_usecase.go
@@ -0,0 +1,32 @@
+package usecase
+
+import (
+	"database/sql"
+
+	"github.com/labstack/echo/v4"
+
+	"problem1/model"
+	"problem1/service"
+)
+
+//go:generate go run github.com/golang/mock/mockgen -source=$GOFILE -destination=../mock/mock_$GOPACKAGE/mock_$GOFILE
+
+type FriendListUseCase interface {
+	GetFriendListByUserId(c echo.Context) ([]*model.FriendList, error)
+}
+
+type friendListUseCase struct {
+	db  *sql.DB
+	fls service.FriendListService
+}
+
+func NewFriendListUseCase(db *sql.DB, fls service.FriendListService) FriendListUseCase {
+	return &friendListUseCase{
+		db:  db,
+		fls: fls,
+	}
+}
+
+func (u *friendListUseCase) GetFriendListByUserId(c echo.Context) ([]*model.FriendList, error) {
+	return u.fls.GetFriendListByUserId(c)
+}
