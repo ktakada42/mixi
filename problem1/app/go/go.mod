4c76c96876a5e31c7afc69090071593300ac9461
ktakada42 <ktakada950711@gmail.com>
2022-09-03 12:01:56 +0900

    test: create test for usecase layer

--- problem1/app/go/go.mod
+++ problem1/app/go/go.mod
@@ -26,10 +26,14 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
+	github.com/yuin/goldmark v1.3.5 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.1 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ problem1/app/go/usecase/friend_list_usecase_test.go
@@ -0,0 +1,102 @@
+package usecase
+
+import (
+	"database/sql"
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+
+	"problem1/mock/mock_service"
+	"problem1/model"
+	"problem1/testutil"
+)
+
+type friendListUseCaseTest struct {
+	db   *sql.DB
+	mock sqlmock.Sqlmock
+	fls  *mock_service.MockFriendListService
+	flu  FriendListUseCase
+}
+
+func newFriendListUseCaseTest(t *testing.T) *friendListUseCaseTest {
+	t.Helper()
+
+	ctrl := gomock.NewController(t)
+	db, mock := testutil.NewSQLMock(t)
+	fls := mock_service.NewMockFriendListService(ctrl)
+	flu := NewFriendListUseCase(db, fls)
+
+	return &friendListUseCaseTest{
+		db:   db,
+		mock: mock,
+		fls:  fls,
+		flu:  flu,
+	}
+}
+
+func newFriendList() []*model.FriendList {
+	return []*model.FriendList{
+		{
+			Id:   111111,
+			Name: "hoge",
+		},
+		{
+			Id:   222222,
+			Name: "fuga",
+		},
+	}
+}
+
+func Test_friendListUseCase_GetFriendListByUesrId(t *testing.T) {
+	want := newFriendList()
+
+	tests := []struct {
+		name    string
+		expects func(test *friendListUseCaseTest)
+		want    []*model.FriendList
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			expects: func(ut *friendListUseCaseTest) {
+				ut.fls.EXPECT().GetFriendListByUserId(gomock.Any()).Return(want, nil)
+			},
+			want:    want,
+			wantErr: false,
+		},
+		{
+			name: "ng: error at GetFriendListByUserId()",
+			expects: func(ut *friendListUseCaseTest) {
+				ut.fls.EXPECT().GetFriendListByUserId(gomock.Any()).Return(nil, testutil.ErrTest)
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ut := newFriendListUseCaseTest(t)
+			tt.expects(ut)
+
+			e := echo.New()
+			e.GET("", nil)
+			url, err := url.Parse("")
+			if err != nil {
+				t.Fatal(err)
+			}
+			c := e.NewContext(&http.Request{URL: url}, nil)
+
+			got, err := ut.flu.GetFriendListByUserId(c)
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("GetFriendListByUserId() error = %v, wantErr = %v", err, tt.wantErr)
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
