a113f83ec53c56abe89e948354ea7151a628e935
ktakada42 <ktakada950711@gmail.com>
2022-09-04 20:13:16 +0900

    feat: add FriendList to model layer

--- problem1/app/go/model/friend_list.go
+++ problem1/app/go/model/friend_list.go
@@ -5,3 +5,8 @@ type User struct {
 	Id   int    `json:"userId" db:"user_id"`
 	Name string `json:"name" db:"name"`
 }
+
+// FriendList OpenAPI: FriendList
+type FriendList struct {
+	Friends []*User `json:"friends"`
+}
