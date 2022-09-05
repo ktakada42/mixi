272bb594dc30cf54c443392480162b2b8d04c7a5
ktakada42 <ktakada950711@gmail.com>
2022-09-05 22:36:43 +0900

    feat: 1hop friends exclude from frined_list_of_friends

--- problem1/app/go/repository/friend_list_repository.go
+++ problem1/app/go/repository/friend_list_repository.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"errors"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 
 	"problem1/model"
@@ -83,31 +84,53 @@ WHERE FL.user1_id = ?`
 func (r *friendListRepository) GetFriendListOfFriendsByUserId(c echo.Context) (*model.FriendList, error) {
 	userId := c.QueryParam("userId")
 
-	const q = `
-	SELECT DISTINCT U.user_id, U.name
-	FROM users AS U
-	INNER JOIN friend_link AS FL
-	ON U.user_id = FL.user2_id
-	INNER JOIN friend_link AS FL2
-	ON FL.user1_id = FL2.user2_id
-	WHERE FL2.user1_id = ?;`
+	// get 1hop friends
+	const q1 = `
+SELECT user2_id
+FROM friend_link
+WHERE user1_id = ?;`
 
-	rows, err := r.db.Query(q, userId)
+	rows, err := r.db.Query(q1, userId)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var friends []*model.User
+	var (
+		oneHopFriends []int
+		oneHopFriend  int
+	)
 	for rows.Next() {
-		friend := &model.User{}
-		if err := rows.Scan(&friend.Id, &friend.Name); err != nil {
+		if err := rows.Scan(&oneHopFriend); err != nil {
 			return nil, err
 		}
 
-		friends = append(friends, friend)
+		oneHopFriends = append(oneHopFriends, oneHopFriend)
 	}
 	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	// get 2hop Friends except for 1hop friends
+	const q2 = `
+	SELECT U.user_id, U.name
+		FROM users AS U
+	INNER JOIN friend_link AS FL
+	ON U.user_id = FL.user2_id
+	INNER JOIN friend_link AS FL2
+	ON FL.user1_id = FL2.user2_id
+	WHERE FL2.user1_id = ?
+	AND U.user_id NOT IN (?);`
+
+	dbx := sqlx.NewDb(r.db, "mysql")
+
+	q, args, err := sqlx.In(q2, userId, oneHopFriends)
+	if err != nil {
+		return nil, err
+	}
+
+	var friends []*model.User
+	if err := dbx.Select(&friends, q, args...); err != nil {
 		return nil, err
 	}
 
