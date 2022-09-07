7e52160596f7ea83695f5ab8a6724aba282807b2
ktakada42 <ktakada950711@gmail.com>
2022-09-07 23:43:37 +0900

    test: add paging test to controller

--- problem1/app/go/controller/friend_list_controller_test.go
+++ problem1/app/go/controller/friend_list_controller_test.go
@@ -211,3 +211,85 @@ func Test_friendListController_GetFriendListOfFriendsByUserId(t *testing.T) {
 		})
 	}
 }
+
+func Test_friendListController_GetFriendListOfFriendsByUserIdWithPaging(t *testing.T) {
+	want := newFriendList()
+
+	tests := []struct {
+		name       string
+		expects    func(test *friendListControllerTest)
+		url        string
+		want       *model.FriendList
+		wantStatus int
+		wantErr    bool
+	}{
+		{
+			name: "ok",
+			expects: func(ct *friendListControllerTest) {
+				ct.flu.EXPECT().GetFriendListOfFriendsByUserIdWithPaging(gomock.Any()).Return(want, nil)
+			},
+			url:        "/get_friend_list?ID=123456789",
+			want:       want,
+			wantStatus: http.StatusOK,
+			wantErr:    false,
+		},
+		{
+			name:       "ng: userId missing in query parameter",
+			expects:    func(ct *friendListControllerTest) {},
+			url:        "/get_friend_list",
+			want:       nil,
+			wantStatus: http.StatusBadRequest,
+			wantErr:    true,
+		},
+		{
+			name:       "ng: userId not integer",
+			expects:    func(ct *friendListControllerTest) {},
+			url:        "/get_friend_list?ID=invalid",
+			want:       nil,
+			wantStatus: http.StatusBadRequest,
+			wantErr:    true,
+		},
+		{
+			name:       "ng: userId minus",
+			expects:    func(ct *friendListControllerTest) {},
+			url:        "/get_friend_list?ID=-1",
+			want:       nil,
+			wantStatus: http.StatusBadRequest,
+			wantErr:    true,
+		},
+		{
+			name:       "ng: userId over mysql max limit",
+			expects:    func(ct *friendListControllerTest) {},
+			url:        "/get_friend_list?ID=999999999999999999",
+			want:       nil,
+			wantStatus: http.StatusBadRequest,
+			wantErr:    true,
+		},
+		{
+			name: "ng: error at GetFriendListOfFriendsByUserId()",
+			expects: func(ct *friendListControllerTest) {
+				ct.flu.EXPECT().GetFriendListOfFriendsByUserIdWithPaging(gomock.Any()).Return(nil, testutil.ErrTest)
+			},
+			url:        "/get_friend_list?ID=0",
+			want:       nil,
+			wantStatus: http.StatusInternalServerError,
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ct := newFriendListControllerTest(t)
+			tt.expects(ct)
+
+			rec, req := httputil.NewRequestAndRecorder("GET", tt.url, nil)
+			ct.echo.GET("/get_friend_list", ct.flc.GetFriendListOfFriendsByUserIdWithPaging)
+			ct.echo.ServeHTTP(rec, req)
+
+			assert.Equal(t, tt.wantStatus, rec.Code)
+			if !tt.wantErr {
+				testutil.AssertResponseBody(t, want, rec.Body)
+			}
+		})
+	}
+}
