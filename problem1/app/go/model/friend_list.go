3a4a19986533b60b517897cfa2268e9b9b0832f7
ktakada42 <ktakada950711@gmail.com>
2022-09-10 08:49:50 +0900

    feat: add UserLinkForRequest to model layer

--- problem1/app/go/model/friend_list.go
+++ problem1/app/go/model/friend_list.go
@@ -1,5 +1,12 @@
 package model
 
+// UserLinkForRequest OpenAPI: UserLinkForRequest
+type UserLinkForRequest struct {
+	User1Id int    `json:"user1Id" db:"user1_id"`
+	User2Id int    `json:"user2Id" db:"user2_id"`
+	Table   string `json:"table"`
+}
+
 // Friend OpenAPI: Friend
 type Friend struct {
 	UserId int    `json:"userId" db:"user_id"`
