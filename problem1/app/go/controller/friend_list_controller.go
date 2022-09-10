395da127fb143af7aef89b2e844383fcb080a3e0
ktakada42 <ktakada950711@gmail.com>
2022-09-10 22:04:23 +0900

    refactor: controller just return error

--- problem1/app/go/controller/friend_list_controller.go
+++ problem1/app/go/controller/friend_list_controller.go
@@ -39,47 +39,37 @@ const (
 func (c *friendListController) PostUserLink(ctx echo.Context) error {
 	var req model.UserLinkForRequest
 	if err := json.NewDecoder(ctx.Request().Body).Decode(&req); err != nil {
-		httputil.RespondError(ctx, err)
-		return err
+		return httputil.NewHTTPError(errors.New("request invalid"), http.StatusBadRequest, "")
 	}
 
 	if req.User1Id < 0 || maxUserId < req.User1Id || req.User2Id < 0 || maxUserId < req.User2Id {
-		err := httputil.NewHTTPError(errors.New("userId is invalid"), http.StatusBadRequest, "")
-		httputil.RespondError(ctx, err)
-		return err
+		return httputil.NewHTTPError(errors.New("userId is invalid"), http.StatusBadRequest, "")
 	}
 
 	switch req.Table {
 	case "friend_link", "block_list":
 		if err := c.friendListUseCase.PostUserLink(&req); err != nil {
-			httputil.RespondError(ctx, err)
 			return err
 		}
 
-		return ctx.NoContent(http.StatusOK)
+		return nil
 	default:
-		err := httputil.NewHTTPError(errors.New("table not exist"), http.StatusBadRequest, "")
-		httputil.RespondError(ctx, err)
-		return err
+		return httputil.NewHTTPError(errors.New("table not exist"), http.StatusBadRequest, "")
 	}
 }
 
 func (c *friendListController) GetFriendListByUserId(ctx echo.Context) error {
 	userId, err := strconv.Atoi(ctx.QueryParam("ID"))
 	if err != nil {
-		httputil.RespondError(ctx, httputil.NewHTTPError(err, http.StatusBadRequest, "userId is not integer or not exist in query parameter"))
-		return err
+		return httputil.NewHTTPError(err, http.StatusBadRequest, "userId is not integer or not exist in query parameter")
 	}
 	if userId < 0 || maxUserId < userId {
-		err := httputil.NewHTTPError(errors.New("userId is invalid"), http.StatusBadRequest, "")
-		httputil.RespondError(ctx, err)
-		return err
+		return httputil.NewHTTPError(errors.New("userId is invalid"), http.StatusBadRequest, "")
 	}
 	ctx.Set("userId", userId)
 
 	friendList, err := c.friendListUseCase.GetFriendListByUserId(ctx)
 	if err != nil {
-		httputil.RespondError(ctx, err)
 		return err
 	}
 
@@ -90,19 +80,15 @@ func (c *friendListController) GetFriendListByUserId(ctx echo.Context) error {
 func (c *friendListController) GetFriendListOfFriendsByUserId(ctx echo.Context) error {
 	userId, err := strconv.Atoi(ctx.QueryParam("ID"))
 	if err != nil {
-		httputil.RespondError(ctx, httputil.NewHTTPError(err, http.StatusBadRequest, "userId is not integer or not exist in query parameter"))
-		return err
+		return httputil.NewHTTPError(err, http.StatusBadRequest, "userId is not integer or not exist in query parameter")
 	}
 	if userId < 0 || maxUserId < userId {
-		err := httputil.NewHTTPError(errors.New("userId is invalid"), http.StatusBadRequest, "")
-		httputil.RespondError(ctx, err)
-		return err
+		return httputil.NewHTTPError(errors.New("userId is invalid"), http.StatusBadRequest, "")
 	}
 	ctx.Set("userId", userId)
 
 	friendList, err := c.friendListUseCase.GetFriendListOfFriendsByUserId(ctx)
 	if err != nil {
-		httputil.RespondError(ctx, err)
 		return err
 	}
 
@@ -113,19 +99,15 @@ func (c *friendListController) GetFriendListOfFriendsByUserId(ctx echo.Context) 
 func (c *friendListController) GetFriendListOfFriendsByUserIdWithPaging(ctx echo.Context) error {
 	userId, err := strconv.Atoi(ctx.QueryParam("ID"))
 	if err != nil {
-		httputil.RespondError(ctx, httputil.NewHTTPError(err, http.StatusBadRequest, "userId is not integer or not exist in query parameter"))
-		return err
+		return httputil.NewHTTPError(err, http.StatusBadRequest, "userId is not integer or not exist in query parameter")
 	}
 	if userId < 0 || maxUserId < userId {
-		err := httputil.NewHTTPError(errors.New("userId is invalid"), http.StatusBadRequest, "")
-		httputil.RespondError(ctx, err)
-		return err
+		return httputil.NewHTTPError(errors.New("userId is invalid"), http.StatusBadRequest, "")
 	}
 	ctx.Set("userId", userId)
 
 	friendList, err := c.friendListUseCase.GetFriendListOfFriendsByUserIdWithPaging(ctx)
 	if err != nil {
-		httputil.RespondError(ctx, err)
 		return err
 	}
 
