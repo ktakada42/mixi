67f00d62d81a5d99bf850f9300f8a7120a0091a9
ktakada42 <ktakada950711@gmail.com>
2022-09-10 14:16:45 +0900

    feat: add PostUserLink() to controller layer

--- problem1/app/go/controller/friend_list_controller.go
+++ problem1/app/go/controller/friend_list_controller.go
@@ -1,11 +1,14 @@
 package controller
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
 
+	"problem1/model"
 	"problem1/pkg/httputil"
 	"problem1/usecase"
 )
@@ -13,6 +16,7 @@ import (
 //go:generate go run github.com/golang/mock/mockgen -source=$GOFILE -destination=../mock/mock_$GOPACKAGE/mock_$GOFILE
 
 type FriendListController interface {
+	PostUserLink(c echo.Context) error
 	GetFriendListByUserId(c echo.Context) error
 	GetFriendListOfFriendsByUserId(c echo.Context) error
 	GetFriendListOfFriendsByUserIdWithPaging(c echo.Context) error
@@ -31,6 +35,39 @@ func NewFriendListController(flu usecase.FriendListUseCase) FriendListController
 const (
 	maxUserId = 4294967295 // max unsigned int at mysql
 )
+
+func (c *friendListController) PostUserLink(ctx echo.Context) error {
+	var req model.UserLinkForRequest
+	if err := json.NewDecoder(ctx.Request().Body).Decode(&req); err != nil {
+		httputil.RespondError(ctx, err)
+		return err
+	}
+
+	if req.User1Id < 0 || maxUserId < req.User1Id {
+		err := httputil.NewHTTPError(errors.New("userId is invalid"), http.StatusBadRequest, "")
+		httputil.RespondError(ctx, err)
+		return err
+	}
+	if req.User2Id < 0 || maxUserId < req.User2Id {
+		err := httputil.NewHTTPError(errors.New("userId is invalid"), http.StatusBadRequest, "")
+		httputil.RespondError(ctx, err)
+		return err
+	}
+
+	switch req.Table {
+	case "friend_link", "block_list":
+		if err := c.friendListUseCase.PostUserLink(&req); err != nil {
+			httputil.RespondError(ctx, err)
+			return err
+		}
+
+		return ctx.NoContent(http.StatusOK)
+	default:
+		err := httputil.NewHTTPError(errors.New("table not exist"), http.StatusBadRequest, "")
+		httputil.RespondError(ctx, err)
+		return err
+	}
+}
 
 func (c *friendListController) GetFriendListByUserId(ctx echo.Context) error {
 	userId, err := strconv.Atoi(ctx.QueryParam("ID"))
