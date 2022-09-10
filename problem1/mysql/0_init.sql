d917f09a5aa9a01f4fea4aad6a0032a762658122
ktakada42 <ktakada950711@gmail.com>
2022-09-10 19:14:28 +0900

    refactor: table accept only unsigned int

--- problem1/mysql/0_init.sql
+++ problem1/mysql/0_init.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`
 (
-    `id`      bigint(20)             NOT NULL AUTO_INCREMENT,
-    `user_id` int(11)                NOT NULL UNIQUE,
+    `id`      bigint(20) unsigned    NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) unsigned       NOT NULL UNIQUE,
     `name`    varchar(64) DEFAULT '' NOT NULL,
     PRIMARY KEY (`id`)
 );
@@ -10,17 +10,17 @@ CREATE TABLE `users`
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `friend_link`
 (
-    `id`       bigint(20) NOT NULL AUTO_INCREMENT,
-    `user1_id` int(11)    NOT NULL,
-    `user2_id` int(11)    NOT NULL,
+    `id`       bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `user1_id` int(11) unsigned    NOT NULL,
+    `user2_id` int(11) unsigned    NOT NULL,
     PRIMARY KEY (`id`)
 );
 -- user1 user2 block
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `block_list`
 (
-    `id`       bigint(20) NOT NULL AUTO_INCREMENT,
-    `user1_id` int(11)    NOT NULL,
-    `user2_id` int(11)    NOT NULL,
+    `id`       bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `user1_id` int(11) unsigned    NOT NULL,
+    `user2_id` int(11) unsigned    NOT NULL,
     PRIMARY KEY (`id`)
 );
