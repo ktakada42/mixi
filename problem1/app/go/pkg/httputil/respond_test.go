6774dfc638cb9fb9104650b56907897018893f6b
ktakada42 <ktakada950711@gmail.com>
2022-09-10 17:24:40 +0900

    test: add RespondJSON() test

--- problem1/app/go/pkg/httputil/respond_test.go
+++ problem1/app/go/pkg/httputil/respond_test.go
@@ -40,3 +40,55 @@ func Test_respond_RespondError(t *testing.T) {
 		})
 	}
 }
+
+type friend struct {
+	userId int    `json:"userId"`
+	name   string `json:"name"`
+}
+
+type friendList struct {
+	friends []*friend `json:"friends"`
+}
+
+func Test_respond_RespondJSON(t *testing.T) {
+	tests := []struct {
+		name     string
+		payload  any
+		want     string
+		wantCode int
+		wantErr  bool
+	}{
+		{
+			name: "ok",
+			payload: &friendList{
+				friends: []*friend{
+					{
+						userId: testutil.UserIDForDebug,
+						name:   testutil.UserNameForDebug,
+					},
+					{
+						userId: testutil.UserIDForDebug,
+						name:   testutil.UserNameForDebug,
+					},
+				},
+			},
+			wantCode: http.StatusOK,
+			wantErr:  false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			c := echo.New().NewContext(nil, rec)
+			RespondJSON(c, tt.wantCode, tt.payload)
+			resp := rec.Result()
+			defer resp.Body.Close()
+			assert.Equal(t, tt.wantCode, resp.StatusCode)
+			if tt.wantErr {
+				// エラー時はBodyを書き換えているのでチェック
+				testutil.AssertResponseBody(t, tt.want, resp.Body)
+			}
+		})
+	}
+}
