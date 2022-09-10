833743bde5101466bf318a556574d63eb38b55ce
ktakada42 <ktakada950711@gmail.com>
2022-09-10 18:48:50 +0900

    refactor: no use openapi validator

--- problem1/app/go/pkg/httputil/httperror_test.go
+++ problem1/app/go/pkg/httputil/httperror_test.go
@@ -9,29 +9,6 @@ import (
 	"problem1/pkg/testutil"
 )
 
-func Test_httpError_SchemaValidation(t *testing.T) {
-	tests := []struct {
-		schemaName string
-		target     any
-	}{
-		{
-			schemaName: "HTTPError",
-			target: &httpError{
-				origin:     testutil.ErrTest,
-				statusCode: http.StatusInternalServerError,
-				message:    testutil.ErrTest.Error(),
-			},
-		},
-	}
-
-	ot := testutil.NewOpenAPITester(t, "../../../spec/openapi.yaml")
-	for _, tt := range tests {
-		t.Run(tt.schemaName, func(t *testing.T) {
-			ot.ValidateBySchema(t, tt.schemaName, tt.target)
-		})
-	}
-}
-
 func Test_httpError_NewHTTPError(t *testing.T) {
 	statusCode := http.StatusInternalServerError
 
--- problem1/app/go/pkg/testutil/json.go
+++ problem1/app/go/pkg/testutil/json.go
@@ -7,17 +7,6 @@ import (
 	"testing"
 )
 
-func I2V(t *testing.T, src, dst interface{}) {
-	t.Helper()
-	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(src); err != nil {
-		t.Fatal(err)
-	}
-	if err := json.NewDecoder(&buf).Decode(&dst); err != nil {
-		t.Fatal(err)
-	}
-}
-
 func I2Reader(t *testing.T, i any) io.Reader {
 	t.Helper()
 
