2bc4802e149c93f80ddcc54dbd2657045271dd50
ktakada42 <ktakada950711@gmail.com>
2022-09-10 17:24:27 +0900

    refactor: no need to check type

--- problem1/app/go/pkg/httputil/respond.go
+++ problem1/app/go/pkg/httputil/respond.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"log"
 	"net/http"
-	"reflect"
 
 	"github.com/labstack/echo/v4"
 )
@@ -24,22 +23,6 @@ func RespondError(c echo.Context, err error) {
 }
 
 func RespondJSON(c echo.Context, status int, payload any) {
-	v := reflect.ValueOf(payload)
-	if v.Kind() == reflect.Slice {
-		// any -> []any に変換
-		payloads := make([]any, v.Len())
-		for i := 0; i < v.Len(); i++ {
-			payloads[i] = v.Index(i).Interface()
-		}
-
-		respondJSON(c, status, payloads)
-		return
-	}
-
-	respondJSON(c, status, payload)
-}
-
-func respondJSON(c echo.Context, status int, payload any) {
 	b, err := json.Marshal(payload)
 	if err != nil {
 		RespondError(c, err)
